dd9f61b7f1bfac392a1a21d47275fb54e2e0f6d7
xspero <jlazarosanchez1@gmail.com>
2017-08-05 10:15:52 -0500

    Add files via upload

--- scriptlauncher.cpp
+++ scriptlauncher.cpp
@@ -17,12 +17,24 @@ void ScriptLauncher::apagarPantalla()
 {
     m_process->start("echo 1 > /sys/class/backlight/rpi_backlight/bl_power");
     m_process->kill();
-    m_process->start("apagar.sh");
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
+    m_process->start("apagar.sh");  //con apagar.sh en /bin
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
     m_process->start("sh apagar.sh");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
     m_process->start("sh /bin/apagar.sh");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
 
 
 }
@@ -31,11 +43,23 @@ void ScriptLauncher::encenderPantalla()
 {
     m_process->start("echo 0 > /sys/class/backlight/rpi_backlight/bl_power");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
     m_process->start("encender.sh");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
     m_process->start("sh encender.sh");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
     m_process->start("sh /bin/encender.sh");
     m_process->kill();
+    m_process->close();
+    m_process->terminate();
+    m_process->waitForFinished(1000);
 
 }
