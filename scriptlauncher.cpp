98724b0714b6b3a842b417ec66e8811e14f33257
xspero <jlazarosanchez1@gmail.com>
2017-08-05 11:12:57 -0500

    Add files via upload

--- scriptlauncher.cpp
+++ scriptlauncher.cpp
@@ -1,4 +1,7 @@
 #include "scriptlauncher.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 ScriptLauncher::ScriptLauncher(QObject *parent) :
 QObject(parent),
@@ -15,51 +18,40 @@ void ScriptLauncher::launchScript()
 
 void ScriptLauncher::apagarPantalla()
 {
-    m_process->start("echo 1 > /sys/class/backlight/rpi_backlight/bl_power");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("apagar.sh");  //con apagar.sh en /bin
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("sh apagar.sh");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("sh /bin/apagar.sh");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
+    bool isOn=true;
+
+    QFile backlightFile("/sys/class/backlight/rpi_backlight/bl_power");
+
+    if (backlightFile.exists()) {
+        if (!backlightFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            //qWarning() << "Can't open backlightfile for write?";
+            return;
+        }
+        QTextStream out(&backlightFile);
+        out << (isOn ? 0 : 1);
+    } else {
+        qDebug() << "Backlight would have been turned " << (isOn ? "on" : "off") << " here";
+    }
 
 
 }
 
 void ScriptLauncher::encenderPantalla()
 {
-    m_process->start("echo 0 > /sys/class/backlight/rpi_backlight/bl_power");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("encender.sh");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("sh encender.sh");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
-    m_process->start("sh /bin/encender.sh");
-    m_process->kill();
-    m_process->close();
-    m_process->terminate();
-    m_process->waitForFinished(1000);
+    bool isOn=false;
+
+    QFile backlightFile("/sys/class/backlight/rpi_backlight/bl_power");
+
+    if (backlightFile.exists()) {
+        if (!backlightFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            //qWarning() << "Can't open backlightfile for write?";
+            return;
+        }
+        QTextStream out(&backlightFile);
+        out << (isOn ? 0 : 1);
+    } else {
+        qDebug() << "Backlight would have been turned " << (isOn ? "on" : "off") << " here";
+    }
+
 
 }
